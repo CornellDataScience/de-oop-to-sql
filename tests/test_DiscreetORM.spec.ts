aedf2f8b6182088006f5386a7724a3e554f717db
Ahad Rizvi <ar727@cornell.edu>
2019-10-27 16:36:05 -0400

    more refactoring

--- tests/test_DiscreetORM.spec.ts
+++ tests/test_DiscreetORM.spec.ts
@@ -3,9 +3,11 @@ import { expect } from 'chai';
 import 'mocha';
 
 describe('empty test bench', () => {
-
     it('empty test', () => {
         const result = null;
+        class TestIO implements DiscreetORMIO {
+            
+        }
         expect(result).to.equal(null);
     });
 
