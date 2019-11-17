31ad4ce27bd7bd10885e898b80e26ee7744e38f1
Haram Kim <kimharam@gmail.com>
2019-11-17 01:07:02 -0500

    fix tests

--- tests/test_DiscreetORM.spec.ts
+++ tests/test_DiscreetORM.spec.ts
@@ -35,10 +35,9 @@ describe('addRow test bench', () => {
             }
         }
         let test_obj = new TestClass();
-        let test_id = DiscreetORM.idOfObject(test_obj);
-        let expected_command = <string>'INSERT\xa0INTO\xa0\'TestClass\'\xa0VALUES\xa0(\'' + test_id + '\',\xa0\'hello\',\xa034);'
+        let expected_command = <string>'INSERT INTO `TestClass` (`a`, `b`) VALUES (\'hello\', 34);';
         let test_obj_sql = <string>DiscreetORM.commandForAddRow(test_obj);
-        expect(spaceIndependentEquals(expected_command, test_obj_sql)).to.be.true;
+        expect(expected_command).to.equal(test_obj_sql);
     });
     it ("addRow doesn't add functions", () => {
         class TestClass{
@@ -53,10 +52,9 @@ describe('addRow test bench', () => {
             }
         }
         let test_obj = new TestClass();
-        let test_id = DiscreetORM.idOfObject(test_obj);
-        let expected_command = <string>'INSERT\xa0INTO\xa0\'TestClass\'\xa0VALUES\xa0(\'' + test_id + '\',\xa0\'hello\',\xa034);'
+        let expected_command = <string>'INSERT INTO `TestClass` (`a`, `b`) VALUES (\'hello\', 34);';
         let test_obj_sql = <string>DiscreetORM.commandForAddRow(test_obj);
-        expect(spaceIndependentEquals(expected_command, test_obj_sql)).to.be.true;
+        expect(expected_command).to.equal(test_obj_sql);
     });
 
 });
